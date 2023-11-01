75955cabc6d7fea608a1b23187db17b3f086d425
mfmealing <megan@mealing.me.uk>
2023-11-01 12:07:06 +0000

    Adds docstrings to milestone_4 code

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -2,7 +2,21 @@ import random
 fruit_list = ["apple", "banana", "pear", "grape", "strawberry"]
 
 class Hangman:
+    '''
+    This class plays a game of hnagman with the user. A random word is selected and the user will input letters until either the word is guessed or all lives are lost
+
+    Attributes:
+    word: The word to be guessed, picked randomly from the word_list. Remember to import the random module into your script
+    word_guessed: list - A list of the letters of the word, with _ for each letter not yet guessed. For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']. If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
+    num_letters: int - The number of UNIQUE letters in the word that have not been guessed yet
+    num_lives: int - The number of lives the player has at the start of the game.
+    word_list: list - A list of words
+    list_of_guesses: list - A list of the guesses that have already been tried. Set this to an empty list initially
+    '''
     def __init__(self, word_list, num_lives=5):
+        '''
+        See help(Hangman) for accurate signature
+        '''
         self.word_list = word_list
         self.num_lives = num_lives
 
@@ -12,6 +26,15 @@ class Hangman:
         self.list_of_guesses = []
     
     def check_guess(self, guess):
+        '''
+        This function checks if the letter input by the user is in the randomly generated word after making it lowercase.
+        If it is, its added to an array which displays _ for unguesed letters and all of the currently guessed letters.
+        If it isn't, a life is lost.
+        The user is told if they are correct or not.
+
+        Args:
+            guess (str): the single letter input by the user to be compared with the word
+        '''
         guess_lower = guess.lower()
         if guess_lower in self.word:
             print("Good guess! ", guess_lower, " is in the word.")
@@ -27,6 +50,11 @@ class Hangman:
 
 
     def ask_for_input(self):
+        '''
+        This function checks if the user input letter is a single alphabetical value.
+        It also checks if the letter has already been guessed before, as all letters that pass the initial check are added to a blank array of guessed letters.
+        This will repeat until a correct input is given.
+        '''
         while True:
             guess = input("Please enter a single letter: ")
             guess_lower = guess.lower()
